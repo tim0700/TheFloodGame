ba112a95448b358ad9cc9564aac6df3f330e8226
정우현 <dngus2063@naver.com>
2025-08-18 21:31:22 +0900

    아이템 선택시 뿌해지는거 구현, 마우스 나오게 함

--- Assets/Scripts/Inventory/ItemSlot.cs
+++ Assets/Scripts/Inventory/ItemSlot.cs
@@ -1,8 +1,10 @@
+//ItemSLot.cs
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class ItemSlot : MonoBehaviour
+public class ItemSlot : MonoBehaviour, IPointerClickHandler
 {
     //====ITEM DATA====//
     public string ItemName;
@@ -16,9 +18,18 @@ public class ItemSlot : MonoBehaviour
 
     [SerializeField]
     private Image ItemImage;
+
+    public GameObject SelectedShader;
+    public bool ThisItemSelected;
+
     private InventoryManger inventoryManger;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    private void Start()
+    {
+        inventoryManger = GameObject.Find("InventoryCanvas").GetComponent<InventoryManger>();
+    }
 
     public void AddItem(string ItemName, int Quantity, Sprite ItemSprite)
     {
@@ -27,9 +38,37 @@ public class ItemSlot : MonoBehaviour
         this.ItemSprite = ItemSprite;
         IsFull = true;
 
-        //QuantityText.text = Quantity.ToString();
-       // QuantityText.enabled = true;
+   
+        QuantityText.text = Quantity.ToString();
+        QuantityText.enabled = true;
         ItemImage.sprite = ItemSprite;
     }
 
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.button == PointerEventData.InputButton.Left)
+        {
+            Debug.Log("item LEFT BUTTON");
+            OnLeftClick();
+        }
+
+        if (eventData.button == PointerEventData.InputButton.Right)
+        {
+            Debug.Log("item Right BUTTON");
+            OnRightClick();
+        }
+    }
+
+    public void OnLeftClick()
+    {
+        inventoryManger.DeselectAllSlot();
+        SelectedShader.SetActive(true);
+        ThisItemSelected = true;
+    }
+
+    public void OnRightClick()
+    {
+        SelectedShader.SetActive(true);
+        ThisItemSelected = true;
+    }
 }
